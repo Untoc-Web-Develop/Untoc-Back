35a167f9c553cee211d1591bf6609f9461ebf4e4
gimdogyun <jasper12394@gmail.com>
2024-02-25 16:37:46 +0900

    feat : global prefix

--- src/swagger.ts
+++ src/swagger.ts
@@ -3,12 +3,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export const setupSwagger = (app: INestApplication): void => {
   const options = new DocumentBuilder()
-    .setBasePath('api')
     .setTitle('Untoc API Docs')
     .setDescription('Untoc 벡엔드 API 문서입니다.')
     .setVersion('1.0.0')
     .build();
 
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document);
+  const document = SwaggerModule.createDocument(app, options, {
+    ignoreGlobalPrefix: false,
+  });
+  SwaggerModule.setup('api/docs', app, document);
 };
