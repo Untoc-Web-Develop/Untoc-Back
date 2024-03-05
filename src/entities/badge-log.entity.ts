e3b5eb6e0dc161220d5fe36725665ec1c20165db
Kim DoKyun <51306225+jasper200207@users.noreply.github.com>
2024-03-06 01:01:35 +0900

    Badge_log_Id change

--- src/entities/badge-log.entity.ts
+++ src/entities/badge-log.entity.ts
@@ -1,10 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Badge } from 'src/entities/badge.entity';
-import { BaseEntity } from 'src/entities/base.entity';
+import { BaseEntityOnlyDate } from 'src/entities/base.entity';
 import { User } from 'src/entities/user.entity';
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity({ name: 'badge_log' })
-export class BadgeLog extends BaseEntity {
+export class BadgeLog extends BaseEntityOnlyDate {
+  @ApiProperty({ description: 'id' })
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @Column({ name: 'is_given' })
   isGiven: boolean;
 
