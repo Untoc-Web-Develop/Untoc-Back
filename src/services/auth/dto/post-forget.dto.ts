392ff4c465b9d2e6f46edc7b02da2c902ab8eabf
kbs1027 <rlaqjatn1027@gmail.com>
2024-03-20 15:09:24 +0900

    chore : description 상세화 - dto description 상세화

--- src/services/auth/dto/post-forget.dto.ts
+++ src/services/auth/dto/post-forget.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ForgetPasswordRequestDto {
-  @ApiProperty({ description: '이메일' })
+  @ApiProperty({ description: '비밀번호 재설정을 위한 확인 코드 전송 이메일' })
   email: string;
 }
